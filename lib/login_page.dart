2f60756a2dca865ec7ba493d714e8bc1a4322956
munem91 <borisukrusaln12345@gmail.com>
2022-11-21 22:26:55 +0300

    Create UI login_page
    Create UI login_page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -11,8 +14,116 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Test 3 Ruslan'),
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Center(
+          // ignore: prefer_const_literals_to_create_immutables
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            // Hello
+            Image.asset('lib/assets/images/logo.png', scale: 1.8),
+            SizedBox(height: 10),
+            Text(
+              'Hello!',
+              style: GoogleFonts.bebasNeue(
+                fontSize: 52,
+              ),
+            ),
+            SizedBox(height: 1),
+            Text(
+              'Welcom back!',
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(height: 25),
+            // email textfield
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 500.0, vertical: 10.0),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    border: Border.all(color: Colors.white),
+                    borderRadius: BorderRadius.circular(15)),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 10.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Email:',
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            // pasword textfield
+            SizedBox(height: 10),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 500.0, vertical: 10.0),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    border: Border.all(color: Colors.white),
+                    borderRadius: BorderRadius.circular(15)),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 10.0),
+                  child: TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Password:',
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            // sign is button
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 500.0, vertical: 10.0),
+              child: Container(
+                padding: const EdgeInsets.all(20.0),
+                decoration: BoxDecoration(
+                  color: Colors.deepPurple,
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                child: Center(
+                  child: Text(
+                    'Sign In',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            // not a member? register now
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              // ignore: prefer_const_literals_to_create_immutables
+              children: [
+                Text(
+                  'Not a member? ',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  'Register now',
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )
+              ],
+            ),
+          ]),
+        ),
       ),
     );
   }
