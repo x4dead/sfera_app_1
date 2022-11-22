26af4ce1762690022c3b908c94c8d6b275e83cbd
x4dead <muratovargen0@gmail.com>
2022-11-22 13:16:53 +0600

    log in

---
+++ lib/widgets/app_button/app_button.dart
@@ -0,0 +1,36 @@
+﻿import 'package:flutter/material.dart';
+
+class AppButton extends StatelessWidget {
+  const AppButton({
+    Key? key,
+    required this.text,
+    required this.onPressed,
+  }) : super(key: key);
+  final String text;
+  final Function() onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 60,
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          primary: Colors.deepPurple,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+        ),
+        child: Text(
+          text,
+          style: const TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+          ),
+        ),
+      ),
+    );
+  }
+}
