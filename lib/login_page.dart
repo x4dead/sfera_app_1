31e206ae29644786544a58e47605ea1570a2c9ef
x4dead <muratovargen0@gmail.com>
2022-11-22 13:29:17 +0600

    log in

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -15,6 +15,7 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
+<<<<<<< Updated upstream
       body: SafeArea(
         child: Center(
           // ignore: prefer_const_literals_to_create_immutables
@@ -123,6 +124,83 @@ class _LoginPageState extends State<LoginPage> {
               ],
             ),
           ]),
+=======
+      body: Center(
+        child: SizedBox(
+          width: 350,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Hello
+              // Image.asset('lib/assets/images/logo.png', scale: 1.8),
+              const SizedBox(height: 10),
+              Text(
+                'Hello again!'.toUpperCase(),
+                style: GoogleFonts.bebasNeue(
+                  fontSize: 52,
+                ),
+              ),
+              const SizedBox(height: 1),
+              const Text(
+                'Welcom back!',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+              const SizedBox(height: 25),
+              // email textfield
+              const AppTextField(
+                icon: Icons.mail_outline_outlined,
+                maxLength: 36,
+                text: 'Email',
+              ),
+              // pasword textfield
+              const SizedBox(height: 10),
+              AppTextField(
+                icon: Icons.lock_outline,
+                maxLength: 16,
+                text: 'Password',
+                suffix: IconButton(
+                  splashRadius: 15,
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.visibility_outlined,
+                    color: AppColors.color000000,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              // sign is button
+              AppButton(
+                onPressed: () {},
+                text: 'Sign In',
+              ),
+              const SizedBox(height: 20),
+              // not a member? register now
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Not a member? ',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () {},
+                    child: const Text(
+                      'Register now',
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+>>>>>>> Stashed changes
         ),
       ),
     );
