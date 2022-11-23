322ac50dc0c359fe4d1a8fd8442bedfb80a4b566
x4dead <muratovargen0@gmail.com>
2022-11-24 01:52:33 +0600

    installation firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,17 @@
-// import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firedart/auth/token_store.dart';
 import 'package:flutter/material.dart';
 import 'package:sfera_app_1/service_locator.dart' as di;
-// import 'package:sfera_app_1/service_locator.dart';
-// import 'package:firebase_core/firebase_core.dart';
-import 'features/presentation/pages/home_page/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'features/presentation/pages/login_page/login_page.dart';
 
 // part 'main_page.dart';
 
 void main() async {
-  await di.init();
+  // await di.init();
   // WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp();
+  FirebaseAuth.instance;
+
   runApp(const MyApp());
 }
 
